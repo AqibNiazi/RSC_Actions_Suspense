8e3e75d0210c7204b0b61875043da26033a171bd
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-26 16:20:06 +0500

    Data fetching with server components

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import ClientDemo from "@/components/client-demo";
+import DataFetching from "@/components/data-fetching";
 import RSCDemo from "@/components/rsc_demo";
 
 export default function Home() {
   return (
     <main>
-      <ClientDemo>
-        <RSCDemo />
-      </ClientDemo>
+      <ClientDemo />
+      <RSCDemo />
+      <DataFetching />
     </main>
   );
 }
