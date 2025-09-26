d245b29ce6f76921a993c820ee953d2ca17b11a9
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-26 16:08:49 +0500

    React Server COmponents and server actions project setup completed

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import ClientDemo from "@/components/client-demo";
+import RSCDemo from "@/components/rsc_demo";
+
+export default function Home() {
+  return (
+    <main>
+      <ClientDemo>
+        <RSCDemo />
+      </ClientDemo>
+    </main>
+  );
+}
